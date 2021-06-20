7fcf439c27b38e162a475131a150c6c84c492f58
Ilia <kukhnovets93@mail.ru>
2021-06-21 02:18:56 +0300

    added Unit test

--- app/src/test/java/com/example/test_bitnet/ExampleUnitTest.kt
+++ app/src/test/java/com/example/test_bitnet/ExampleUnitTest.kt
@@ -3,6 +3,10 @@ package com.example.test_bitnet
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +17,14 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun cast_current_date() {
+        val cal = Calendar.getInstance()
+        val dateTime = cal.time
+        val dateFormat = SimpleDateFormat("yyyy-MM", Locale.getDefault())
+        val formatted = dateFormat.format(dateTime)
+        assertEquals("2021-06", formatted)
     }
 }
